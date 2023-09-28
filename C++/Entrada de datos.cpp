7efadb4e1a0763a0ff65cb9896e2cbc28e287f3a
c4rlos302 <144201005+c4rlos302@users.noreply.github.com>
2023-09-28 14:54:05 -0600

    Create Entrada de datos.cpp

---
+++ C++/Entrada de datos.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string palabra;
+    int entero;
+    float flotante;
+    bool boleano;
+    
+    cout<<"Dame una palabra"<<endl;
+    cin >> palabra;
+    
+    cout<<"Dame un entero"<<endl;
+    cin >> entero;
+    
+    cout<<"Dame un flotante"<<endl;
+    cin >> flotante;
+    
+    cout<<"Dame un boleano"<<endl;
+    cin >> boleano;
+    
+    cout <<"Tu palabra es: "<<palabra<<endl;
+    cout <<"Tu enflotantetero es: "<<entero<<endl;
+    cout <<"Tu flotante es: "<<flotante<<endl;
+    cout <<"Tu boleano es: "<<boleano<<endl;
+    
+    return 0;
+}
