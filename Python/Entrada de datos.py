6987f46e1e430d16786b41b8daf17dff88a926df
c4rlos302 <144201005+c4rlos302@users.noreply.github.com>
2023-09-28 15:07:25 -0600

    Create Entrada de datos.py

---
+++ Python/Entrada de datos.py
@@ -0,0 +1,21 @@
+palabra = str(input("Escribe una palabra\n"))
+entero = int(input("Escribe un entero\n"))
+flotante = float(input("Escribe un flotante\n"))
+boleano = bool(input("Escribe un boleano\n"))
+print("\n")
+
+print ("Tu palabra es:",palabra)
+print ("Tu palabra es de tipo:",type(palabra))
+print("\n")
+
+print ("Tu entero es:",entero)
+print ("Tu entero es de tipo:",type(entero))
+print("\n")
+
+print ("Tu flotante es:",flotante)
+print ("Tu flotante es de tipo:",type(flotante))
+print("\n")
+
+print ("Tu boleano es:",boleano)
+print ("Tu boleano es de tipo:",type(boleano))
+print("\n")
