cfdcf87f22259620c49c79830424a9e30fd6e9c4
c4rlos302 <144201005+c4rlos302@users.noreply.github.com>
2023-09-28 19:34:25 -0600

    Create Menú en Switch.c

---
+++ C/Menú en Switch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int dia;
+    puts("Elige un dia de la semana del 1 al 7");
+    scanf("%d",&dia);
+    switch(dia)
+    {
+        case 1:puts("Elegiste el Lunes");
+            break;
+        
+        case 2:puts("Elegiste el Martes");
+            break;
+            
+        case 3:puts("Elegiste el Miercoles");
+            break;
+        
+        case 4:puts("Elegiste el Jueves");
+            break;
+            
+        case 5:puts("Elegiste el Viernes");
+            break;
+        
+        case 6:puts("Elegiste el Sabado");
+            break;
+        
+        case 7:puts("Elegiste el Domingo");
+            break;
+        
+        default:puts("¡Error!");
+    }
+    return 0;
+}
